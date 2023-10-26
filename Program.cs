be4e7474c8fa8f44bbe987f04da9098d5fbba1b9
thabrkci <alpacinotaha@gmail.com>
2023-10-26 11:28:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace İf_else_kullanımı;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+        if(time>6 && time<12)
+        Console.WriteLine( "Günaydin!");
+        else if(time<= 18)
+        Console.WriteLine("İyi Günler!");
+        else
+        Console.WriteLine("İyi geceler!");
+
+        string sonuc = time<=18 ? "İyi günler!" : "İyi geceler!";
+        sonuc = time>=6 && time<12 ? "Günaydin" : time<=18 ? "İyi Günler!" :"İyi Geceler";
+        Console.WriteLine(sonuc);
+
+    
+
+
+        
+    }
+}
